7f54935d3b836c4ed8dc1dacb008988e87324912
Benjamin Bulk <benjamin@bulk-dev.com>
2023-04-03 20:37:45 +0200

    updated to a better example

--- src/test/java/dev/bbulk/rest_client_for_testing/MyRestClient.java
+++ src/test/java/dev/bbulk/rest_client_for_testing/MyRestClient.java
@@ -1,10 +1,14 @@
 package dev.bbulk.rest_client_for_testing;
 
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
 @RegisterRestClient
 public interface MyRestClient {
+
+    @POST
+    void createSomeInformation(String information);
 
     @GET
     String getSomeInformation();
--- src/test/java/dev/bbulk/rest_client_for_testing/MyRestClientTest.java
+++ src/test/java/dev/bbulk/rest_client_for_testing/MyRestClientTest.java
@@ -1,20 +1,34 @@
 package dev.bbulk.rest_client_for_testing;
 
 import org.eclipse.microprofile.rest.client.RestClientBuilder;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.net.URI;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MyRestClientTest {
+
+    private static MyRestClient restClient;
+
+    @BeforeAll
+    public static void init() {
+        restClient = RestClientBuilder.newBuilder()
+                .baseUri(URI.create("https://example.com/"))
+                .build(MyRestClient.class);
+    }
 
     @Test
     public void callSomeUrlTest() {
-        MyRestClient restClient = RestClientBuilder.newBuilder()
-                .baseUri(URI.create("https://example.com/"))
-                .build(MyRestClient.class);
+        // given
+        restClient.createSomeInformation("myTestData");
 
+        // when
         String information = restClient.getSomeInformation();
-        System.out.println(information);
+
+        // then
+        assertEquals("myTestData", information);
     }
 
 }
